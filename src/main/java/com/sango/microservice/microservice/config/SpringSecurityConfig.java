a7f7397ba3447dd414c154c55fae085e68f79578
Sangopak <sangojumech07@gmail.com>
2018-09-22 22:15:51 -0400

    Added BCrypt Password encoder

--- src/main/java/com/sango/microservice/microservice/config/SpringSecurityConfig.java
+++ src/main/java/com/sango/microservice/microservice/config/SpringSecurityConfig.java
@@ -1,16 +1,44 @@
 package com.sango.microservice.microservice.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
-
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		PasswordEncoder passwordEncoder = new PasswordEncoder() {
+			
+			@Override
+			public boolean matches(CharSequence rawPassword, String encodedPassword) {
+				return BCrypt.checkpw(rawPassword.toString(), encodedPassword);
+			}
+			
+			@Override
+			public String encode(CharSequence rawPassword) {
+				return BCrypt.hashpw(rawPassword.toString(), BCrypt.gensalt(4));
+			}
+		};
+	
+		return passwordEncoder;
+	}
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 			http
@@ -25,9 +53,13 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth)
-			throws Exception {
-		auth.inMemoryAuthentication().passwordEncoder(NoOpPasswordEncoder.getInstance())
-				.withUser("user1").password("secret1").roles("USER")
-				.and().withUser("admin1").password("secret1").roles("USER", "ADMIN");
+			throws Exception {		
+		auth.inMemoryAuthentication().passwordEncoder(passwordEncoder())
+				.withUser("user").password("$2a$04$7InhLUCC7G36qwKQusRig.Bu3VD94bMM9oByLdrRRf8mnNyQvjN1e").roles("USER")
+				.and().withUser("admin").password("$2a$04$7InhLUCC7G36qwKQusRig.Bu3VD94bMM9oByLdrRRf8mnNyQvjN1e").roles("USER", "ADMIN");
+	}
+	
+	public static void main(String args[]) {
+		System.out.println(BCrypt.hashpw("enter the password", BCrypt.gensalt(4)));
 	}
 }
